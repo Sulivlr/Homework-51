a10e78141b98a4d4e3a7785644129b947bdbcb4a
Sultan Mukhtarov <suli419956@gmail.com>
2023-12-21 19:58:00 +0600

    1. Created vite, installed dependencies. 2. Initial commit. Created Lottery.tsx. 3. second initial commit. Created a code with a button that updates numbers with on clicks. 4. Now created a cycle of numbers that generates from numbers [5, 11, 16, 23, 32] + 5. So, at the very moment, when button is clicked, there will be some random numbers from 5 to 36.

--- src/Lottery/Lottery.tsx
+++ src/Lottery/Lottery.tsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
 
 interface Props {
-    UpdateCircles: () => number;
+    updateCircles: () => number;
 }
 
-const Lottery: React.FC<Props> = ({UpdateCircles}) => {
- return (
-     <div className="lottery">
-         <h2>Lottery</h2>
-         <div className="circles">
-             <p>current circle numbers</p>
-         </div>
-         <button onClick={UpdateCircles}>New numbers</button>
-     </div>
- );
+const Lottery: React.FC<Props> = ({ updateCircles }) => {
+    const [currentNumbers, setCurrentNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const generateNewNumbers = () => {
+        const newNumbers: number[] = [];
+
+        while (newNumbers.length < 5) {
+            const randomNumber = Math.floor(Math.random() * 32) + 5;
+            if (!newNumbers.includes(randomNumber)) {
+                newNumbers.push(randomNumber);
+            }
+        }
+
+        setCurrentNumbers([...newNumbers.sort((a, b) => a - b)]);
+    };
+
+    return (
+        <div className="lottery">
+            <h2>Lottery</h2>
+            <div className="circles">
+                <p>Current circle numbers: {currentNumbers.join(', ')}</p>
+            </div>
+            <button onClick={() => { generateNewNumbers(); updateCircles(); }}>New numbers</button>
+        </div>
+    );
 };
 
 export default Lottery;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Lottery from "./Lottery/Lottery.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Lottery updateCircles={function(): number {
+            throw new Error('Function not implemented.');
+        } } />
   </React.StrictMode>,
 )
