413ff45d94d8fcaa093755e791ab1c23337e7b63
Sultan Mukhtarov <suli419956@gmail.com>
2023-12-21 19:06:09 +0600

    Created vite, installed dependencies. Initial commit. Created Lottery.tsx. second initial commit. Created a code with a button that updates numbers with on clicks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css'
-import Lottery from "./Lottery/Lottery.tsx";
-import {useState} from "react";
 
 function App() {
 
--- src/Lottery/Lottery.tsx
+++ src/Lottery/Lottery.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 interface Props {
-    circles: number;
+    UpdateCircles: () => number;
 }
 
-const Lottery: React.FC<Props> = ({circles}) => {
-
-}
+const Lottery: React.FC<Props> = ({UpdateCircles}) => {
+ return (
+     <div className="lottery">
+         <h2>Lottery</h2>
+         <div className="circles">
+             <p>current circle numbers</p>
+         </div>
+         <button onClick={UpdateCircles}>New numbers</button>
+     </div>
+ );
+};
 
 export default Lottery;
